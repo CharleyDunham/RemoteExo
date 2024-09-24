7f9137acc599db8f7c28deaf9acfc40552c84c8e
Charley Dunham <charles-dunham@uiowa.edu>
2024-09-24 08:34:45 -0500

    Created guess method that checks if valid input as letter, checks for letters that have already been guessed, and adds them to the correct list

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -3,6 +3,7 @@ class WordGuesserGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
   attr_accessor :word, :guesses, :wrong_guesses
+
   # Get a word from remote "random word" service
 
   def initialize(word)
@@ -10,6 +11,22 @@ class WordGuesserGame
     @guesses = ''
     @wrong_guesses = ''
   end
+
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter.empty? || !letter.match?(/[a-zA-Z]/)
+
+    # checks for letters that are already guessed
+    return true if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+
+    # adds the letter to the guesses if it is in the word
+    @guesses += letter if @word.include?(letter)
+    # adds to wrong guesses if it is not in the word
+    @wrong_guesses += letter unless @word.include?(letter)
+
+    true
+  end
+
+
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
@@ -20,7 +37,7 @@ class WordGuesserGame
     require 'net/http'
     uri = URI('http://randomword.saasbook.info/RandomWord')
     Net::HTTP.new('randomword.saasbook.info').start { |http|
-      return http.post(uri, "").body
+      return http.post(uri, '').body
     }
   end
 
