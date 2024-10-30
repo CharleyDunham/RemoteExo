9cb25ccd8d41f4d58c0abf5cf90a4f1c642c5d65
sullivan-bryant <sbryant1@uiowa.edu>
2024-10-30 13:34:57 -0500

    Update force_sensor_class.cpp

--- Arduino Docs/force_sensor_class.cpp
+++ Arduino Docs/force_sensor_class.cpp
@@ -1,7 +1,6 @@
 #include "force_sensor_class.h"
 
-force_sensor::force_sensor(unsigned int& pin_no, const bool monitor_input, const unsigned long delay) : m_pin_no(pin_no), m_monitor_input(monitor_input), m_sample_delay(delay) 
-{
+force_sensor::force_sensor(unsigned int& pin_no, const bool monitor_input, const unsigned long delay) : m_pin_no(pin_no), m_monitor_input(monitor_input), m_sample_delay(delay) {
 	m_prev_time = 0;
 }
 
@@ -13,24 +12,19 @@ void force_sensor::update() {
 	if (!m_monitor_input) {
 		return;
 	}
-	
-	
 	if (m_current_time - m_prev_time < m_sample_delay) {
 		return;
 	}
-	
+	//since both conditions fail, update previous time, resistance, and force.
 	m_prev_time = m_current_time;
-	
-	
-	
 	m_resistance = static_cast<float>((m_k_r / analogRead(m_pin_no)) - 1);
-
 	m_force = static_cast<float>((m_k_f * m_resistance));
 }
 
 void force_sensor::set_delay(const unsigned long delay) {
 	if (delay > 10000) {
-		Serial.println("Delay too long");
+	//delay limit
+		Serial.println("Delay is too long");
 		return;
 	}
 
